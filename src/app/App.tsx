504985a5b8f2890566e451a6cb4e820ea9747a41
VareraJan <vareraj@gmail.com>
2023-06-03 14:44:53 +0300

    init

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,15 @@
 import { Suspense } from "react";
+import { useTranslation } from "react-i18next";
 import { classNames } from "../shared/lib/classNames/classNames";
 import { useTheme } from "../shared/lib/hooks/useTheme/useTheme";
 import { AppRouter } from "./Providers/router";
 import "./styles/index.scss";
+
+const Component = () => {
+  const { t, i18n } = useTranslation();
+
+  return <h1>{t("Привет")}</h1>;
+};
 
 export const App = () => {
   const { theme } = useTheme();
@@ -10,6 +17,7 @@ export const App = () => {
     <div className={classNames("app", {}, [theme])}>
       <Suspense fallback="">
         <h2>APP 1</h2>
+        <Component />
         <AppRouter />
       </Suspense>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import { App } from "./app/App";
 import { ThemeProvider } from "./app/Providers/ThemeProvider";
 import { BrowserRouter } from "react-router-dom";
 
+import './shared/config/i18n/i18n'
+
 const container = document.getElementById("root");
 
 if (!container) {
