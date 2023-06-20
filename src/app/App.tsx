c44cf1d7418111b823de00c13ddf95470eca029d
VareraJan <vareraj@gmail.com>
2023-06-21 01:03:29 +0300

    добавил eslint, prettier и первоначальную настройку

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,23 @@
-import { Header } from "@/shared/ui/Header";
-import { AppLayout } from "@/shared/ui/Layout";
-import { Navbar } from "@/widgets/Navbar";
-import { Suspense } from "react";
-import { classNames } from "../shared/lib/classNames/classNames";
-import { useTheme } from "../shared/lib/hooks/useTheme/useTheme";
-import { AppRouter } from "./Providers/router";
-import "./styles/index.scss";
+import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
+import { Header } from '@/shared/ui/Header';
+import { AppLayout } from '@/shared/ui/Layout';
+import { Navbar } from '@/widgets/Navbar';
+import { Suspense } from 'react';
+import { classNames } from '../shared/lib/classNames/classNames';
+import { AppRouter } from './Providers/router';
+import './styles/index.scss';
 
 export const App = () => {
-  const { theme } = useTheme();
-  return (
-    <AppLayout>
-      <div className={classNames("app", {}, [theme])}>
-        <Suspense fallback="">
-          <Header />
-          <Navbar />
-          <AppRouter />
-        </Suspense>
-      </div>
-    </AppLayout>
-  );
+    const { theme } = useTheme();
+    return (
+        <AppLayout>
+            <div className={classNames('app', {}, [theme])}>
+                <Suspense fallback="">
+                    <Header />
+                    <Navbar />
+                    <AppRouter />
+                </Suspense>
+            </div>
+        </AppLayout>
+    );
 };
